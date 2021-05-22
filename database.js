95e1a572568ed463e571853b31404738b3fd002b
rafifauz <rafitugas3@gmail.com>
2021-05-22 21:33:11 +0700

    v1

---
+++ database.js
@@ -0,0 +1,26 @@
+// // Initialize Firebase
+// var config = {
+//     apiKey: "AIzaSyDgXAHnBMIGF75Eo3IzL16VYhkcOoJ_H2g ",
+//     authDomain: "web-app-1905.firebaseapp.com ",
+//     projectId: "web-app-1905 ",
+//     storageBucket: "web-app-1905.appspot.com ",
+//     messagingSenderId: "830957746059 ",
+//     appId: "1:830957746059:web:5b65caee519f8b02fd218e "
+// };
+// firebase.initializeApp(config);
+// const db = firebase.firestore();
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyDgXAHnBMIGF75Eo3IzL16VYhkcOoJ_H2g",
+    authDomain: "web-app-1905.firebaseapp.com",
+    projectId: "web-app-1905",
+    storageBucket: "web-app-1905.appspot.com",
+    messagingSenderId: "830957746059",
+    appId: "1:830957746059:web:5b65caee519f8b02fd218e"
+
+
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
