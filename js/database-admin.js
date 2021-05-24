59df00a2cd16f52f877f2fcb8b55c6021d3d93c3
rafifauz <rafitugas3@gmail.com>
2021-05-24 22:25:57 +0700

    add Icon

--- js/database-admin.js
+++ js/database-admin.js
@@ -1,10 +1,23 @@
+// // Initialize Firebase
+// var config = {
+//     apiKey: "AIzaSyDgXAHnBMIGF75Eo3IzL16VYhkcOoJ_H2g ",
+//     authDomain: "web-app-1905.firebaseapp.com ",
+//     projectId: "web-app-1905 ",
+//     storageBucket: "web-app-1905.appspot.com ",
+//     messagingSenderId: "830957746059 ",
+//     appId: "1:830957746059:web:5b65caee519f8b02fd218e "
+// };
+// firebase.initializeApp(config);
+// const db = firebase.firestore();
+
+// Your web app's Firebase configuration
 var firebaseConfig = {
     apiKey: "AIzaSyDgXAHnBMIGF75Eo3IzL16VYhkcOoJ_H2g",
-    authDomain: "process.env.authDomain_admin",
-    projectId: "web-app-1905 ",
-    storageBucket: "process.env.storageBucket_admin",
-    messagingSenderId: "process.env.messagingSenderId_admin",
-    appId: "process.env.appId_admin"
+    authDomain: "web-app-1905.firebaseapp.com",
+    projectId: "web-app-1905",
+    storageBucket: "web-app-1905.appspot.com",
+    messagingSenderId: "830957746059",
+    appId: "1:830957746059:web:5b65caee519f8b02fd218e"
 
 
 };
--- js/database.js
+++ js/database.js
@@ -1,10 +1,24 @@
+// // Initialize Firebase
+// var config = {
+//     apiKey: "AIzaSyDgXAHnBMIGF75Eo3IzL16VYhkcOoJ_H2g ",
+//     authDomain: "web-app-1905.firebaseapp.com ",
+//     projectId: "web-app-1905 ",
+//     storageBucket: "web-app-1905.appspot.com ",
+//     messagingSenderId: "830957746059 ",
+//     appId: "1:830957746059:web:5b65caee519f8b02fd218e "
+// };
+// firebase.initializeApp(config);
+// const db = firebase.firestore();
+
+// Your web app's Firebase configuration
 var firebaseConfig = {
     apiKey: "AIzaSyDET1lBkmp5HuKb5jXgyPD2nD8qXZ5IXt4",
-    authDomain: "process.env.authDomain",
+    authDomain: "hera1234.firebaseapp.com",
+    databaseURL: "https://hera1234-default-rtdb.asia-southeast1.firebasedatabase.app",
     projectId: "hera1234",
-    storageBucket: "process.env.storageBucket",
-    messagingSenderId: "process.env.messagingSenderId",
-    appId: "process.env.appId"
+    storageBucket: "hera1234.appspot.com",
+    messagingSenderId: "1076423867054",
+    appId: "1:1076423867054:web:e01beef4cbbb413eba8b3f"
 
 
 };
